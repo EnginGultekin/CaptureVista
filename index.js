c9cd08941a13f01d0a617387a20e39992e63b8b1
Engin Gültekin <engingultekin1071@gmail.com>
2023-07-13 11:28:15 +0300

    Static files uploaded

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from "express";
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 
+// Middlewares
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.send('Welcome Photo App');
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 })
 
 const port = 3000;
