4d322d8f250f16e5607b370c30508caad1a84335
Engin Gültekin <engingultekin1071@gmail.com>
2023-07-13 09:03:24 +0300

    Express and nodemon package installed and index.js started.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const fn = function () {
-    console.log("-------");
-}
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome Photo App');
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}...`);
+});
